8efac9f8a40ac5b57aa1847e6744a200acba33a2
Shahina-0440 <achukatlashahina@gmail.com>
2023-10-05 10:20:17 +0530

    datatypes in js

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+var num=123; //number
+console.log(typeof num)
+var num1="1.25";//number
+console.log(typeof num1)
+var name1="shahina";//string
+console.log(typeof name1)
+var village;//undefined
+console.log(typeof village)
+var arr=[1,2,3,4];//object-array
+console.log(typeof arr)
+var obj={
+    eid:101,
+    ename:"shahina"
+}//object
+console.log(typeof obj)
